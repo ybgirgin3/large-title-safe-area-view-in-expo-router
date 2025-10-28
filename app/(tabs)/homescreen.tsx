c0a2a27a2c51e6bb57e972c494f8898e5944a2bf
ybgirgin3 <ybgirgin3@gmail.com>
2025-10-29 02:10:09 +0300

    Special for you flatlist

--- app/(tabs)/homescreen.tsx
+++ app/(tabs)/homescreen.tsx
@@ -1,7 +1,8 @@
 import LargeTitleSafeArea from "@/components/large-header-scroll-view";
 import { IconSymbol } from "@/components/ui/icon-symbol.ios";
+import { BigStationCardListMock } from "@/constants/mockdata";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
@@ -22,24 +23,80 @@ export default function HomeScreen() {
         </Pressable>
       }
     >
-      <View style={{ width: "100%", height: 300, backgroundColor: "purple" }}>
-        <Text>HomeScreeennnnn</Text>
-      </View>
-      <View style={{ width: "100%", height: 300, backgroundColor: "gray" }}>
-        <Text>HomeScreeennnnn</Text>
-      </View>
-      <View style={{ width: "100%", height: 300, backgroundColor: "brown" }}>
-        <Text>HomeScreeennnnn</Text>
-      </View>
-      <View style={{ width: "100%", height: 300, backgroundColor: "yellow" }}>
-        <Text>HomeScreeennnnn</Text>
-      </View>
-      <View style={{ width: "100%", height: 300, backgroundColor: "yellow" }}>
-        <Text>HomeScreeennnnn</Text>
-      </View>
-      <View style={{ width: "100%", height: 300, backgroundColor: "yellow" }}>
-        <Text>HomeScreeennnnn</Text>
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          paddingTop: 5,
+          paddingBottom: 5,
+        }}
+      >
+        <BigStationCardList data={BigStationCardListMock} />
       </View>
     </LargeTitleSafeArea>
   );
 }
+
+// components
+const BigStationCardList = ({ data }: { data: any }) => {
+  const BigStationCardRenderItem = ({ item }: { item: any }) => {
+    console.log(item);
+
+    return (
+      <View
+        style={{
+          width: 300,
+          height: 400,
+          // backgroundColor: "purple",
+          // borderRadius: radius,
+          marginRight: 15,
+        }}
+      >
+        {/* image background */}
+        <View
+          style={{
+            width: "100%",
+            height: "80%",
+            backgroundColor: item.imageBackground,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+          }}
+        ></View>
+
+        {/* title */}
+        <View
+          style={{
+            width: "100%",
+            height: "20%",
+            backgroundColor: item.titleBackground,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+
+            // child
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            numberOfLines={2}
+            style={{ color: "white", fontSize: 18, fontWeight: "bold" }}
+          >
+            {item.title}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      data={data}
+      style={{ paddingLeft: 15 }}
+      showsHorizontalScrollIndicator={false}
+      renderItem={BigStationCardRenderItem}
+      horizontal={true}
+    />
+  );
+};
+
---
+++ constants/mockdata.tsx
@@ -0,0 +1,37 @@
+export const BigStationCardListMock = [
+  {
+    title: "Discover Station",
+    titleBackground: "blue",
+    imageBackground: "yellow",
+  },
+  {
+    title: "DARK LIGTH ALBUM NEW SONGS",
+    titleBackground: "#048791ff",
+    imageBackground: "gold",
+  },
+  {
+    title: "3rd one",
+    titleBackground: "#2e3535ff",
+    imageBackground: "#2ec1c1ff",
+  },
+  {
+    title: "3rd one",
+    titleBackground: "#2e3535ff",
+    imageBackground: "#2ec1c1ff",
+  },
+    {
+    title: "3rd one",
+    titleBackground: "#2e3535ff",
+    imageBackground: "#2ec1c1ff",
+  },
+    {
+    title: "3rd one",
+    titleBackground: "#2e3535ff",
+    imageBackground: "#2ec1c1ff",
+  },
+    {
+    title: "3rd one",
+    titleBackground: "#2e3535ff",
+    imageBackground: "#2ec1c1ff",
+  },
+];
